4d31e54b9c8f6f018cf8d4a0ba091e24c8940fe7
Ivan <ivan.kuchkov17@gmail.com>
2020-11-23 15:06:39 +0200

    commit

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -4,6 +4,9 @@ import {LoginComponent} from './login/login.component';
 import {AdminComponent} from './admin/admin.component';
 import {HomeComponent} from './home/home.component';
 import {ManagerComponent} from './manager/manager.component';
+import {EditUserComponent} from './edit-user/edit-user.component';
+import {UsersListComponent} from './users-list/users-list.component';
+import {UsersComponent} from './users/users.component';
 import {MainLibraryListActionsComponent} from './main-library-list-actions/main-library-list-actions.component';
 
 
@@ -12,7 +15,9 @@ const routes: Routes = [
   { path: 'admin', component: AdminComponent},
   { path: 'manager', component: ManagerComponent},
   { path: 'auth/login', component: LoginComponent},
-  { path: 'home', component: HomeComponent, pathMatch: 'full'}
+  { path: 'home', component: HomeComponent, pathMatch: 'full'},
+  { path: 'edituser/:id', component: EditUserComponent },
+  { path: 'users', component: UsersComponent}
 ];
 
 @NgModule({
--- src/app/services/user.service.ts
+++ src/app/services/user.service.ts
@@ -1,22 +1,53 @@
 import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
 import { Observable } from 'rxjs';
+import { User } from '../model/user';
+import { UserDto } from '../users-list/user-dto';
+import {Params} from '@angular/router';
 
 @Injectable({
   providedIn: 'root'
 })
 export class UserService {
+  private url = 'https://automatically-testing-java.herokuapp.com/';
+  //private url = 'http://localhost:8080/';
+  private managerUrl = this.url + 'manager';
+  private adminUrl = this.url + 'admin';
+  private getUsersListUrl = this.url + 'users/list';
+  private countPagesUrl = this.url + 'users/pages/count';
   private managerUrl = 'http://localhost:8080/manager';
   //private adminUrl = 'https://automatically-testing-java.herokuapp.com/admin';
   private adminUrl = 'http://localhost:8080/admin';
 
-  constructor(private http: HttpClient) { }
+  constructor(private http: HttpClient) {
+  }
 
   getManagerBoard(): Observable<string> {
-    return this.http.get(this.managerUrl, { responseType: 'text' });
+    return this.http.get(this.managerUrl, {responseType: 'text'});
   }
 
   getAdminBoard(): Observable<string> {
-    return this.http.get(this.adminUrl, { responseType: 'text' });
+    return this.http.get(this.adminUrl, {responseType: 'text'});
+  }
+
+  getUserById(id: number): Observable<User> {
+    const url = `${this.url}users/${id}`;
+    return this.http.get<User>(url);
+    // return this.http.get<User>(`${this.url}/users/${id}`);
+  }
+  updateUser(user: User) {
+    const url = `${this.url}users/updateUser`;
+    const body = {userId: user.userId, email: user.email, name: user.name, surname: user.surname, role: user.role, enabled: user.enabled};
+    return this.http.post(url, body).toPromise();
+  }
+
+  getPage(paramsVal: Params) {
+    return this.http.get<UserDto[]>(this.getUsersListUrl, {
+      params: paramsVal
+    });
+  }
+
+  countPages() {
+    return this.http.get<number>(this.countPagesUrl);
   }
 }
