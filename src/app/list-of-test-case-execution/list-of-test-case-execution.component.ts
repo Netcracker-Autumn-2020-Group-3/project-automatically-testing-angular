10dad52b717b9399bb27a8b0117e4ca6c572d773
EugeneFi1 <51033432+EugeneFi1@users.noreply.github.com>
2020-12-19 17:03:25 +0200

    list of test case execution refactoring

--- src/app/list-of-test-case-execution/list-of-test-case-execution.component.ts
+++ src/app/list-of-test-case-execution/list-of-test-case-execution.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import {TestCaseExecutionService} from "../services/test-case-execution.service";
 import {TestCaseExecutionWithFailedActionNumber} from "../model/testCaseExecutionWithFailedActionNumber";
+import {newArray} from "@angular/compiler/src/util";
 
 @Component({
   selector: 'app-list-of-test-case-execution',
@@ -11,18 +12,16 @@ export class ListOfTestCaseExecutionComponent implements OnInit {
 
   testCaseExecutions: TestCaseExecutionWithFailedActionNumber[];
 
-  search = {
-    testCaseName: '', projectName: '', status: '', sortField: 'id', pageSize: '3', sortOrder: 'ASC'
-  };
-
   step: number = 15;
-
   start: number = 0;
   end: number = this.step;
-  orderByValue: string = 'id';
-  orderByClause: string = 'ASC';
 
-  searchedTestCaseExecution: any;
+  orderByValue: string = 'id';
+  orderByClause: string = 'DESC';
+  testCaseName: string;
+  projectName: string;
+  status = {passed: true, failed: true};
+
   numberOfTestCaseExecution: number;
   numberOfPage: number;
   pagination: any;
@@ -35,11 +34,12 @@ export class ListOfTestCaseExecutionComponent implements OnInit {
       this.numberOfPage = Math.ceil(this.numberOfTestCaseExecution / this.step);
       this.pagination = new Array(this.numberOfPage);
     });
-    this.getAllTestCaseExecution(this.step, this.start, this.orderByValue, this.orderByClause).subscribe(data => this.testCaseExecutions = data);
+    this.getAllTestCaseExecution(this.step, this.start, this.orderByValue, this.orderByClause, this.testCaseName, this.projectName).subscribe(data => this.testCaseExecutions = data);
   }
 
-  getAllTestCaseExecution(limit: number, offset: number, orderBy: string, orderBuClause: string) {
-    return this.service.getAllTestCaseExecutionWithFailedActionNumber(limit, offset, orderBy, orderBuClause);
+  getAllTestCaseExecution(limit: number, offset: number, orderBy: string, orderBuClause: string, testCaseName: string, projectName: string) {
+    let getStatus = this.statusSearch(this.status.passed, this.status.failed);
+    return this.service.getAllTestCaseExecutionWithFailedActionNumber(limit, offset, orderBy, orderBuClause, testCaseName, projectName, getStatus);
   }
 
   countTestCaseExecutions() {
@@ -50,7 +50,7 @@ export class ListOfTestCaseExecutionComponent implements OnInit {
     if(this.start != 0) {
       this.start = this.start - this.step;
       this.end = this.end - this.step;
-      this.getAllTestCaseExecution(this.step, this.start, this.orderByValue, this.orderByClause).subscribe(data => this.testCaseExecutions = data);
+      this.getAllTestCaseExecution(this.step, this.start, this.orderByValue, this.orderByClause, this.testCaseName, this.projectName).subscribe(data => this.testCaseExecutions = data);
     }
   }
 
@@ -58,25 +58,43 @@ export class ListOfTestCaseExecutionComponent implements OnInit {
     if(this.end <= this.numberOfTestCaseExecution - 1) {
       this.start = this.start + this.step;
       this.end = this.end + this.step;
-      this.getAllTestCaseExecution(this.step, this.start, this.orderByValue, this.orderByClause).subscribe(data => this.testCaseExecutions = data);
+      this.getAllTestCaseExecution(this.step, this.start, this.orderByValue, this.orderByClause, this.testCaseName, this.projectName).subscribe(data => this.testCaseExecutions = data);
     }
   }
 
   onPage(index: number) {
     this.start = index * this.step;
     this.end = this.step + index * this.step;
-    this.getAllTestCaseExecution(this.step, this.start, this.orderByValue, this.orderByClause).subscribe(data => this.testCaseExecutions = data);
+    this.getAllTestCaseExecution(this.step, this.start, this.orderByValue, this.orderByClause, this.testCaseName, this.projectName).subscribe(data => this.testCaseExecutions = data);
   }
+
   onSearchSubmit() {
 
+    if(this.testCaseName=='') {
+      this.testCaseName = 'undefined';
+    }
+    if(this.projectName=='') {
+      this.projectName = 'undefined';
+    }
+    this.getAllTestCaseExecution(this.step, this.start, this.orderByValue, this.orderByClause, this.testCaseName, this.projectName).subscribe(data => this.testCaseExecutions = data);
   }
-  /*orderBy(column: string, orderByClause: string) {
-    this.orderByValue = column;
-    this.getAllTestCaseExecution(this.step, this.start, this.orderByValue).subscribe(data => this.testCaseExecutions = data);
-  }*/
+
   orderBy(value: string) {
     this.orderByClause = value.split(".")[0];
     this.orderByValue = value.split(".")[1];
-    this.getAllTestCaseExecution(this.step, this.start, this.orderByValue, this.orderByClause).subscribe(data => this.testCaseExecutions = data);
+    this.getAllTestCaseExecution(this.step, this.start, this.orderByValue, this.orderByClause, this.testCaseName, this.projectName).subscribe(data => this.testCaseExecutions = data);
+  }
+
+  statusSearch(passed: boolean, failed: boolean): string {
+    if(passed && failed) {
+      return  'all';
+    } else if (!passed && !failed){
+      return  '!all';
+    }
+    if(passed && !failed) {
+      return 'passed';
+    } else {
+      return 'failed';
+    }
   }
 }
